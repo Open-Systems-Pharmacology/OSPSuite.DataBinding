311defa99cc9faba62f2169624c2c3197b7d44c9
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-03-22 07:00:38 -0400

    update verbosity and copyright date

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 [assembly: AssemblyCompany("Open Systems Pharmacology Community")]
 [assembly: AssemblyProduct("OSPSuite.DataBinding")]
-[assembly: AssemblyCopyright("Copyright © 2002-2019 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2024 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
